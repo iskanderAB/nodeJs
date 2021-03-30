44c6d8b63b03b583d1b71aae58db5aa0edf25af4
iskanderAB <iskanderabasi@gmail.com>
2021-03-30 23:43:27 +0100

    create first real server

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const http = require('http');
 
- 
-http.createServer((request , response)=>{ 
-    console.log("hello iskander");
-}).listen(3000);
+
+const server = http.createServer((request , response)=>{
+    response.setHeader('Content-Type','text/html');
+    response.write(`<html>
+                            <head>
+                                <title>my first html page</title>
+                            </head>
+                            <body> 
+                                <h1> hello iskander you are running 10 ly NodeJs server  </h1>
+                            </body></html>`
+                    )
+    response.end();
+    console.log("hello");
+});
+
+server.listen(3000);
